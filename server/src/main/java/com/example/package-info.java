6f2e67eafd913b7b77176afcbb8f8a32d6a0e7ed
Your Name <precado999@gmail.com>
2018-09-27 12:22:21 -0700

    checkstate changes3

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,25 +1,7 @@
+/**
+ * This is package name.
+ */
+
 package com.example;
 
-/**
- * This is a class.
- */
-public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
-
-  /**
-  
-  * @param someone the name of person
-  * @return greeting string
-  */
-  
-
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
-}
